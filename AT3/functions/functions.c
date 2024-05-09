fa03bc8224a7292834cd92af737535421dc25dbb
G3rin <rogerion@gmail.com>
2024-05-09 13:10:52 -0300

    Corrigindo o return da função buscando hospede

--- AT3/functions/functions.c
+++ AT3/functions/functions.c
@@ -7,7 +7,7 @@
 #include "../include/types.h"
 #include "../include/functions.h"
 
-// Lê o arquivo e salva as informações na struct Quarto;
+// Lï¿½ o arquivo e salva as informaï¿½ï¿½es na struct Quarto;
 void lerArquivo(FILE* arquivo, Quarto* quarto, int* qtdQuartos) {
 	while(fscanf(arquivo, "%d;%[^;\n];", &quarto[*qtdQuartos].num, quarto[*qtdQuartos].status) != EOF) {
 		quarto[*qtdQuartos].qtdHospede = 0;
@@ -22,7 +22,7 @@ void lerArquivo(FILE* arquivo, Quarto* quarto, int* qtdQuartos) {
 	}
 }
 
-// Pega as informações da struct Quarto e salva no arquivo;
+// Pega as informaï¿½ï¿½es da struct Quarto e salva no arquivo;
 void salvarArquivo(FILE* arquivo, Quarto* quarto, int qtdQuartos) {
 	arquivo = fopen(CAMINHO_ARQUIVO, "w");
 
@@ -49,7 +49,7 @@ void salvarArquivo(FILE* arquivo, Quarto* quarto, int qtdQuartos) {
 }
 
 void exibirQuartosDisponiveis(Quarto* quarto, int qtdQuartos) {
-	printf("Quartos disponíveis:\n");
+	printf("Quartos disponï¿½veis:\n");
 	for(int i = 0; i < qtdQuartos; i++) {
 		if(strcmp(quarto[i].status, "Disponivel") == 0) {
 			printf(" %d |", quarto[i].num);
@@ -59,32 +59,32 @@ void exibirQuartosDisponiveis(Quarto* quarto, int qtdQuartos) {
 }
 
 void adicionarHospede(Quarto* quarto, int qtdQuartos) {
-   // Exibindo quartos disponíveis;
+   // Exibindo quartos disponï¿½veis;
 	exibirQuartosDisponiveis(quarto, qtdQuartos);
 
 	int numQuarto = 0;
 
-	printf("Insira o número do quarto: ");
+	printf("Insira o nï¿½mero do quarto: ");
 	while(scanf("%d", &numQuarto) != 1 || numQuarto < 1 || numQuarto > QTD_MAX_QUARTOS) {
-		printf("Número inválido. Insira um número válido: ");
+		printf("Nï¿½mero invï¿½lido. Insira um nï¿½mero vï¿½lido: ");
 		while(getchar() != '\n');
 	}
 
-	// Verificando se o quarto está disponível;
+	// Verificando se o quarto estï¿½ disponï¿½vel;
 	if(strcmp(quarto[numQuarto - 1].status, "Disponivel") == 0) {
 		Hospede* hospede = (Hospede*) malloc(sizeof(Hospede));
 
 		if(hospede == NULL) {
-			printf("Erro ao alocar memória. Encerrando o programa...");
+			printf("Erro ao alocar memï¿½ria. Encerrando o programa...");
 			exit(1);
 		} else {
 			printf("Digite o nome do hospede: ");
 			while(scanf(" %[^\n]", hospede->nome) != 1) {
-				printf("Erro ao ler o nome do hóspede, tente novamente: ");
+				printf("Erro ao ler o nome do hï¿½spede, tente novamente: ");
 				while(getchar() != '\n');
 			};
 
-			// Adicionando hóspede ao quarto e incrementando a quantidade total de hóspedes do quarto;
+			// Adicionando hï¿½spede ao quarto e incrementando a quantidade total de hï¿½spedes do quarto;
 			quarto[numQuarto - 1].hospede[quarto[numQuarto - 1].qtdHospede] = *hospede;
 			quarto[numQuarto - 1].qtdHospede++;
 
@@ -92,10 +92,10 @@ void adicionarHospede(Quarto* quarto, int qtdQuartos) {
 			if(quarto[numQuarto - 1].qtdHospede == (QTD_MAX_HOSPEDE / QTD_MAX_QUARTOS)) strcpy(quarto[numQuarto - 1].status, "Ocupado");
 		}
 
-		// Liberando a memória do hospede;
+		// Liberando a memï¿½ria do hospede;
 		free(hospede);
 	} else {
-		printf("Quarto não disponível, tente novamente.\n");
+		printf("Quarto nï¿½o disponï¿½vel, tente novamente.\n");
 	}
 }
 
@@ -132,12 +132,12 @@ void quickSort(Hospede* hospede, int left, int right) {
 	}
 }
 
-// Aloca todos os hóspedes em uma struct Hospede;
+// Aloca todos os hï¿½spedes em uma struct Hospede;
 Hospede* guardarHospedes(Quarto* quarto, int qtdQuartos, int* qtdTotalHospedes) {
 	Hospede* hospede = (Hospede*) malloc(QTD_MAX_HOSPEDE * sizeof(Hospede));
 
 	if(hospede == NULL) {
-		printf("Erro ao alocar memória. Encerrando o programa...\n");
+		printf("Erro ao alocar memï¿½ria. Encerrando o programa...\n");
 		exit(1);
 	} else {
 		for(int i = 0; i < qtdQuartos; i++) {
@@ -153,31 +153,32 @@ Hospede* guardarHospedes(Quarto* quarto, int qtdQuartos, int* qtdTotalHospedes) 
 }
 
 void listarHospedes(Hospede* hospede, int qtdHospedes) {
-	printf("Hóspedes:\n\n");
+	printf("Hï¿½spedes:\n\n");
 
 	for(int i = 0; i < qtdHospedes; i++) {
-		printf("%dº - %s\n", i + 1, hospede[i].nome);
+		printf("%dï¿½ - %s\n", i + 1, hospede[i].nome);
 	}
 
 	printf("\n");
 }
 
 
-// TODO: Arrumar o retorno da função e excluir printf() dela;
-char* buscarHospedePorNome(Quarto* quarto, int qtdQuartos) {
-	char nome[MAX_CHAR], nomeNovo[MAX_CHAR];
+// TODO: Arrumar o retorno da funï¿½ï¿½o e excluir printf() dela;
+Hospede buscarHospedePorNome(Quarto* quarto, int qtdQuartos) {
+	char nome[MAX_CHAR];
 	int encontrado = 0;
+	Hospede hospedeencontrado;
 
 	printf("Digite o nome do hospede a ser buscado: ");
 	scanf(" %[^\n]", nome);
 
 	for(int i = 0; i < qtdQuartos; i++) {
 		for(int j = 0; j < quarto[i].qtdHospede; j++) {
-			if(strcmp(quarto[i].hospede[j].nome, nome) == 0) {
-				strcpy(nomeNovo, quarto[i].hospede[j].nome);
-				printf("Hóspede encontrado:\n");
+			if(strcmp(quarto[i].hospede[j].nome, nome) == 0) {			
+				printf("Hï¿½spede encontrado:\n");
 				printf("Quarto: %d\n", quarto[i].num);
 				printf("Nome: %s\n", quarto[i].hospede[j].nome);
+				
 				encontrado = 1;
 				break;
 			}
@@ -186,9 +187,9 @@ char* buscarHospedePorNome(Quarto* quarto, int qtdQuartos) {
 	}
 
 	if(!encontrado) {
-		printf("Hóspede não encontrado.\n");
+		printf("Hï¿½spede nï¿½o encontrado.\n");
 	}
-	return nomeNovo;
+	return hospedeencontrado;
 }
 
 void quartoVazio(Quarto* quarto, int qtdHospede) {
@@ -202,9 +203,9 @@ void quartoVazio(Quarto* quarto, int qtdHospede) {
   }
 }
 
-//--------------------------------------- FUNÇÕES PARA FINS DE DESENVOLVIMENTO ---------------------------------------//
+//--------------------------------------- FUNï¿½ï¿½ES PARA FINS DE DESENVOLVIMENTO ---------------------------------------//
 
-// Alocar quartos no arquivo (Aloca todos os quartos como disponível e deleta os hóspedes);
+// Alocar quartos no arquivo (Aloca todos os quartos como disponï¿½vel e deleta os hï¿½spedes);
 void alocarQuartos(FILE* arquivo) {
 	arquivo = fopen(CAMINHO_ARQUIVO, "w");
 
@@ -220,15 +221,15 @@ void alocarQuartos(FILE* arquivo) {
 	fclose(arquivo);
 }
 
-// Exibe informações da struct Quarto;
+// Exibe informaï¿½ï¿½es da struct Quarto;
 void exibirQuartos(Quarto* quarto, int qtdQuartos) {
 	for(int i = 0; i < qtdQuartos; i++) {
-		printf("Nº do quarto: %d\n", quarto[i].num);
+		printf("Nï¿½ do quarto: %d\n", quarto[i].num);
 		printf("Status: %s\n", quarto[i].status);
-		printf("Nº de hóspedes: %d\n", quarto[i].qtdHospede);
+		printf("Nï¿½ de hï¿½spedes: %d\n", quarto[i].qtdHospede);
 
 		if(quarto[i].qtdHospede > 0) {
-			printf("Hóspedes:\n");
+			printf("Hï¿½spedes:\n");
 			for(int j = 0; j < quarto[i].qtdHospede; j++) {
 				printf(" - %s\n", quarto[i].hospede[j].nome);
 			}
@@ -256,7 +257,7 @@ void pausarTela() {
 	getchar();
 }
 
-// Configura o ambiente de execução;
+// Configura o ambiente de execuï¿½ï¿½o;
 void configurarAmbiente() {
 	#ifdef _WIN32
 	system("color 0A");
@@ -266,16 +267,16 @@ void configurarAmbiente() {
 	char* local = setlocale(LC_ALL, NULL);
 
 	printf("Configurando a localidade:\n\n");
-	printf("Localidade padrão do sistema: %s\n", local);
+	printf("Localidade padrï¿½o do sistema: %s\n", local);
 
 	if(local != NULL && strcmp(local, "Portuguese_Brazil.1252") == 0) {
-		printf("Localidade já está configurada!\n\n");
+		printf("Localidade jï¿½ estï¿½ configurada!\n\n");
 	} else {
 		#ifdef _WIN32
-		printf("Configurando a localidade para Português do Brasil...\n");
+		printf("Configurando a localidade para Portuguï¿½s do Brasil...\n");
 		printf("Localidade configurada para %s com sucesso!\n\n", setlocale(LC_ALL, "Portuguese"));
 		#elif __linux__
-		printf("Configurando a localidade para Português do Brasil...\n");
+		printf("Configurando a localidade para Portuguï¿½s do Brasil...\n");
 		printf("Localidade configurada para %s com sucesso!\n\n", setlocale(LC_ALL, "pt_BR.UTF-8"));
 		#endif
 	}
@@ -284,3 +285,4 @@ void configurarAmbiente() {
 }
 
 //-------------------------------------------------------------------------------------------------------------------//
+
